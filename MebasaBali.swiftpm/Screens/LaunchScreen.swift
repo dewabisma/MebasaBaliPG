a6b0e6e829a64a7b4a07319b2949858d6f29dc40
Dewa Bisma <idewagedebisma@gmail.com>
2023-04-18 06:38:26 +0700

    done animating launch screen

--- MebasaBali.swiftpm/Screens/LaunchScreen.swift
+++ MebasaBali.swiftpm/Screens/LaunchScreen.swift
@@ -9,23 +9,31 @@ import SwiftUI
 
 struct LaunchScreen: View {
     @Binding var isLaunching: Bool
+    @State var isAnimating = false
+    
     var body: some View {
         VStack(spacing: 0) {
             Image("Logo")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 400)
+                .shadow(color: Color("Blue300"), radius: isAnimating ? 24 : 0)
+                .animation(.linear.repeatForever().speed(0.3), value: isAnimating)
             
                 Text("MebasaBali")
                 .font(.system(size: 88, weight: .bold, design: .rounded))
                 .foregroundColor(.white)
+                .shadow(color: Color("Blue300"), radius: isAnimating ? 24 : 0)
+                .animation(.linear.repeatForever().speed(0.3), value: isAnimating)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(LinearGradient(colors: [Color("Blue600"), Color("Blue500")], startPoint: .leading, endPoint: UnitPoint.trailing))
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                 isLaunching = false
             }
+            
+            isAnimating = true
         }
     }
 }
